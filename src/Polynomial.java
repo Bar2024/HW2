037fc9c775101b19ec1a947f1798694d5e4bcf82
Bar Ben Tsion <bbar@campus.technion.ac.il>
2023-05-30 00:04:20 +0300

    deriative need to be done.. not finished it

---
+++ src/Polynomial.java
@@ -0,0 +1,129 @@
+public class Polynomial extends Function{
+
+    private final double[] coefficients;
+
+    public Polynomial(double... coefficients) {
+        this.coefficients = coefficients;
+    }
+    @Override
+    public double valueAt(double x) {
+        double result = 0.0;
+        for (int i=0; i < coefficients.length; i++) {
+            result += coefficients[i] * Math.pow(x,i);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+
+        boolean isZero;
+        boolean isInteger;
+        boolean isOne;
+        boolean isNegative;
+        StringBuilder polynomial = new StringBuilder();
+
+
+        if(coefficients[0] != 0 && coefficients.length >= 1) {
+            isNegative = coefficients[0] < 0 ? true : false;
+            isInteger = Math.floor(coefficients[0]) == coefficients[0] ? true : false;
+            if(isNegative) {
+                polynomial.append("-");
+            }
+            if(isInteger) {
+                polynomial.append((int)coefficients[0]);
+            } else
+                polynomial.append(coefficients[0]);
+        }
+
+
+        if (coefficients[1] != 0&& coefficients.length >= 2) {
+
+            isNegative = coefficients[1] < 0 ? true : false;
+            isInteger = Math.floor(coefficients[1]) == coefficients[1] ? true : false;
+            isOne = Math.abs(coefficients[1]) == 1 ? true : false;
+
+            if (isNegative) {
+                polynomial.append("-");
+            } else {
+                polynomial.append("+");
+            }
+
+            if (isOne) {
+                polynomial.append("x");
+            }
+
+            if(isInteger && !isOne) {
+                polynomial.append((int)coefficients[1] + "x");
+            }
+
+            if(!isInteger) {
+                polynomial.append(coefficients[1] + "x");
+            }
+
+    }
+
+
+        for(int i=2; i<coefficients.length; i++) {
+
+            isNegative = coefficients[i] < 0 ? true : false;
+            isInteger = Math.floor(coefficients[i]) == coefficients[i] ? true : false;
+            isOne = Math.abs(coefficients[i]) == 1 ? true : false;
+            isZero = coefficients[i] == 0 ? true : false;
+
+            if(isZero) {
+                continue;
+            }
+
+            if (isNegative) {
+                polynomial.append("-");
+            } else {
+                polynomial.append("+");
+            }
+
+            if (isOne) {
+                polynomial.append("x^" + i);
+            }
+
+            if(isInteger && !isOne) {
+                polynomial.append((int)coefficients[i] + "x^" + i);
+            }
+
+            if(!isInteger) {
+                polynomial.append(coefficients[i] + "x^" + i);
+            }
+        }
+        //
+
+        if(polynomial.length() == 0)
+            return "(0)";
+        else
+            return "(" + polynomial + ")";
+    }
+
+    @Override
+    public String derivative() {
+
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+
+    }
+
+
+    @Override
+    public double newtonRaphsonMethod(double a) {
+
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double epsilon) {
+
+    }
+
+    @Override
+    public String taylorPolynomial(int n) {
+
+    }
+}
